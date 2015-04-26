83a43c127ba8bdb6514ae4bdd8267211c605e7b0
miceuz <mic@wemakethings.net>
2015-04-26 10:13:01 +0300

    Move thermistor code to the separate module

--- src/main.c
+++ src/main.c
@@ -2,83 +2,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
-//#include "thermistor.h"
+#include "thermistor.h"
 
 
-#define POINTS_COUNT 20
-
-typedef struct {
-    int temp;
-    int microvolts;
-} temp_point;
-
-static temp_point thermistorPoints[] = {
-    { 850 , 129 },
-    { 800 , 146 },
-    { 750 , 165 },  
-    { 700 , 186 },  
-    { 650 , 210 },  
-    { 600 , 236 },  
-    { 550 , 267 },  
-    { 500 , 300 },  
-    { 450 , 337 },
-    { 400 , 376 },  
-    { 350 , 419 },  
-    { 300 , 464 },  
-    { 200 , 559 },  
-    { 250 , 511 },   
-    { 150 , 608 },  
-    { 100 , 656 },
-    { 50  , 703 },  
-    { 0    , 748 },  
-    { -50 , 789 },  
-    { -100, 828 },  
-    { -150, 862 },  
-    { -200, 892 }
-};
-
-static inline int interpolate(int val, int rangeStart, int rangeEnd, int valStart, int valEnd) {
-    return (rangeEnd - rangeStart) * (val - valStart) / (valEnd - valStart) + rangeStart;
-}
-
-static inline int interpolateVoltage(int temp, unsigned char i){
-    return interpolate(temp, thermistorPoints[i-1].microvolts, thermistorPoints[i].microvolts, thermistorPoints[i-1].temp, thermistorPoints[i].temp);
-}
-
-static inline int interpolateTemperature(int microvolts, unsigned char i){
-//    printf("%d [%d -- %d] // [%d -- %d]\n", microvolts, thermistorPoints[i-1].temp, thermistorPoints[i].temp, thermistorPoints[i-1].microvolts, thermistorPoints[i].microvolts);
-    return interpolate(microvolts, thermistorPoints[i-1].temp, thermistorPoints[i].temp, thermistorPoints[i-1].microvolts, thermistorPoints[i].microvolts);
-}
-
-/**
- * Returns the index of the first point whose temperature value is greater than argument
- **/
-static inline unsigned char searchTemp(int temp) {
-    unsigned char i;
-    for(i = 0; i < POINTS_COUNT; i++) {
-        if(thermistorPoints[i].temp > temp) {
-            return i;
-        }
-    }
-    return POINTS_COUNT-1;
-}
-
-/**
- * Returns the index of the first point whose microvolts value is greater than argument
- **/
-static inline unsigned char searchMicrovolts(int microvolts) {
-    unsigned char i;
-    for(i = 0; i < POINTS_COUNT; i++) {
-        if(thermistorPoints[i].microvolts > microvolts) {
-            return i;
-        }
-    }
-    return POINTS_COUNT-1;
-}
-
-long thermistorLsbToTemperature(unsigned int lsb) {
-    return interpolateTemperature(lsb, searchMicrovolts(lsb));
-}
 
 
 inline static void ledOn() {
@@ -94,7 +20,6 @@ inline static void ledSetup(){
 }
 
 #define DRIVER_ENABLE PA0
-//#define UBRRVAL F_CPU/(16 * BAUD) + 1
 
 inline static void serialSetup() {
     #define BAUD 9600
@@ -144,11 +69,6 @@ uint16_t adcReadChannel(uint8_t channel) {
 
 char buff[10];
 
-// static int64_t toTemperature(int64_t x) {
-//     int64_t res = x*x*x*-25119L/100000000000L + x*x*4336623L/10000000000L + x * -346093150L/1000000000L + 122L;
-//     return  res;
-// }
-
 int main (void) {
     ledSetup();
     stdout = &uart_output;
@@ -169,29 +89,28 @@ int main (void) {
         fputs(" -> ", stdout);
         
         int tt = thermistorLsbToTemperature(thermistor);
-//        fprintf("%d\n", tt, stdout);
         itoa((int)tt, buff, 10);
         fputs(buff, stdout);
         
-        // uint16_t caph = adcReadChannel(CHANNEL_CAPACITANCE_HIGH);
-        // uint16_t capl = adcReadChannel(CHANNEL_CAPACITANCE_LOW);
+        uint16_t caph = adcReadChannel(CHANNEL_CAPACITANCE_HIGH);
+        uint16_t capl = adcReadChannel(CHANNEL_CAPACITANCE_LOW);
 
-        // fputs("Capacitance = ", stdout);
-        // itoa(caph, buff, 10);
-        // fputs(buff, stdout);
-        // fputs(" - ", stdout);
+        fputs("Capacitance = ", stdout);
+        itoa(caph, buff, 10);
+        fputs(buff, stdout);
+        fputs(" - ", stdout);
 
-        // itoa(capl, buff, 10);
-        // fputs(buff, stdout);
-        // fputs(" = ", stdout);
+        itoa(capl, buff, 10);
+        fputs(buff, stdout);
+        fputs(" = ", stdout);
 
-        // itoa(caph - capl, buff, 10);
-        // fputs(buff, stdout);
+        itoa(caph - capl, buff, 10);
+        fputs(buff, stdout);
         
-        // uint16_t chipTemp = adcReadChannel(CHANNEL_CHIP_TEMP);
-        // fputs(" @ temp = ", stdout);
-        // itoa(chipTemp, buff, 10);
-        // fputs(buff, stdout);
+        uint16_t chipTemp = adcReadChannel(CHANNEL_CHIP_TEMP);
+        fputs(" @ temp = ", stdout);
+        itoa(chipTemp, buff, 10);
+        fputs(buff, stdout);
         puts(".");
 
         ledOff();
--- src/thermistor.c
+++ src/thermistor.c
@@ -2,83 +2,88 @@
 
 #define POINTS_COUNT 22
 
-typedef	struct {
-	long temp;
-	unsigned long microvolts;
+typedef struct {
+    int temp;
+    int lsb;
 } temp_point;
 
 static temp_point thermistorPoints[] = {
-	{ 850 , 129 },
-	{ 800 , 146 },
-	{ 750 , 165 },  
-	{ 700 , 186 },  
-	{ 650 , 210 },  
-	{ 600 , 236 },  
-	{ 550 , 267 },  
-	{ 500 , 300 },  
-	{ 450 , 337 },
-	{ 400 , 376 },  
-	{ 350 , 419 },  
-	{ 300 , 464 },  
-	{ 200 , 559 },  
-	{ 250 , 511 },   
-	{ 150 , 608 },  
-	{ 100 , 656 },
-	{ 50  , 703 },  
-	{ 0    , 748 },  
-	{ -50 , 789 },  
-	{ -100, 828 },  
-	{ -150, 862 },  
-	{ -200, 892 }
+    { 850 , 129 },
+    { 800 , 146 },
+    { 750 , 165 },  
+    { 700 , 186 },  
+    { 650 , 210 },  
+    { 600 , 236 },  
+    { 550 , 267 },  
+    { 500 , 300 },  
+    { 450 , 337 },
+    { 400 , 376 },  
+    { 350 , 419 },  
+    { 300 , 464 },  
+    { 200 , 559 },  
+    { 250 , 511 },   
+    { 150 , 608 },  
+    { 100 , 656 },
+    { 50  , 703 },  
+    { 0    , 748 },  
+    { -50 , 789 },  
+    { -100, 828 },  
+    { -150, 862 },  
+    { -200, 892 }
 };
 
-static inline long interpolate(long val, long rangeStart, long rangeEnd, long valStart, long valEnd) {
-//    printf("%ld + (%ld - %ld) * (%ld - %ld) / (%ld - %ld)\n", rangeStart, rangeEnd, rangeStart, val, valStart, valEnd, valStart);
+static inline int interpolate(int val, int rangeStart, int rangeEnd, int valStart, int valEnd) {
     return (rangeEnd - rangeStart) * (val - valStart) / (valEnd - valStart) + rangeStart;
 }
 
-static inline long interpolateVoltage(unsigned long temp, unsigned char i){
-    return interpolate(temp, thermistorPoints[i-1].microvolts, thermistorPoints[i].microvolts, thermistorPoints[i-1].temp, thermistorPoints[i].temp);
+static inline int interpolateVoltage(int temp, unsigned char i){
+    return interpolate(temp, thermistorPoints[i-1].lsb, thermistorPoints[i].lsb, thermistorPoints[i-1].temp, thermistorPoints[i].temp);
 }
 
-static inline long interpolateTemperature(unsigned long microvolts, unsigned char i){
-//	printf("%d [%d -- %d] // [%d -- %d]\n", microvolts, thermistorPoints[i-1].temp, thermistorPoints[i].temp, thermistorPoints[i-1].microvolts, thermistorPoints[i].microvolts);
-    return interpolate(microvolts, thermistorPoints[i-1].temp, thermistorPoints[i].temp, thermistorPoints[i-1].microvolts, thermistorPoints[i].microvolts);
+static inline int interpolateTemperature(int lsb, unsigned char i){
+//    printf("%d [%d -- %d] // [%d -- %d]\n", lsb, thermistorPoints[i-1].temp, thermistorPoints[i].temp, thermistorPoints[i-1].lsb, thermistorPoints[i].lsb);
+    return interpolate(lsb, thermistorPoints[i-1].temp, thermistorPoints[i].temp, thermistorPoints[i-1].lsb, thermistorPoints[i].lsb);
 }
 
 /**
  * Returns the index of the first point whose temperature value is greater than argument
  **/
-static inline unsigned char searchTemp(unsigned long temp) {
-	unsigned char i;
-	for(i = 0; i < POINTS_COUNT; i++) {
-		if(thermistorPoints[i].temp > temp) {
-			return i;
-		}
-	}
-	return POINTS_COUNT-1;
+static inline unsigned char searchTemp(int temp) {
+    unsigned char i;
+    for(i = 0; i < POINTS_COUNT; i++) {
+        if(thermistorPoints[i].temp > temp) {
+            return i;
+        }
+    }
+    return POINTS_COUNT-1;
 }
 
 /**
- * Returns the index of the first point whose microvolts value is greater than argument
+ * Returns the index of the first point whose lsb value is greater than argument
  **/
-static inline unsigned char searchMicrovolts(unsigned long microvolts) {
-	unsigned char i;
-	for(i = 0; i < POINTS_COUNT; i++) {
-		if(thermistorPoints[i].microvolts > microvolts) {
-			return i;
-		}
-	}
-	return POINTS_COUNT-1;
+static inline unsigned char searchLsb(int lsb) {
+    unsigned char i;
+    for(i = 0; i < POINTS_COUNT; i++) {
+        if(thermistorPoints[i].lsb > lsb) {
+            return i;
+        }
+    }
+    return POINTS_COUNT-1;
 }
 
-long thermistorLsbToTemperature(unsigned long lsb) {
-	return interpolateTemperature(lsb, searchMicrovolts(lsb));
+long thermistorLsbToTemperature(unsigned int lsb) {
+	if(lsb >= thermistorPoints[POINTS_COUNT-1].lsb) {
+		return thermistorPoints[POINTS_COUNT-1].temp;
+	}
+	if(lsb <= thermistorPoints[0].lsb) {
+		return thermistorPoints[0].temp;
+	}
+    return interpolateTemperature(lsb, searchLsb(lsb));
 }
 
-void main() {
-	int i;
-	for(i = 128; i < 893; i++) {
-		printf("%d = %ld\n", i, thermistorLsbToTemperature(i));
-	}
-}
+// void main() {
+// 	int i;
+// 	for(i = 129; i < 1024; i++) {
+// 		printf("%d, \n", thermistorLsbToTemperature(i));
+// 	}
+// }
