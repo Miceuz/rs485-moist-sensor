a7f33bb3b5f25501669a9756888bcec02c121672
miceuz <mic@hardcore.lt>
2017-10-12 12:43:13 +0300

    Cleanup test scirpt

--- utils/test.py
+++ utils/test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+
+"""Waits for the sensor to appear on /dev/ttyUSB5, then reads moisture and temperature from it continuously"""
+
 import minimalmodbus
 import serial
 from time import sleep
@@ -8,17 +11,13 @@ minimalmodbus.CLOSE_PORT_AFTER_EACH_CALL = True
 minimalmodbus.PARITY=serial.PARITY_NONE
 minimalmodbus.STOPBITS = 2
 minimalmodbus.BAUDRATE=19200
-# sensor.debug=True
-#print("writing")
-#sensor.write_register(4, value=0, functioncode=6)
-#print("reading holding")
-# print(sensor.read_register(4, functioncode=3))
-# print(sensor.read_register(0, functioncode=4))
-# print(sensor.read_register(1, functioncode=4))
+#minimalmodbus.TIMEOUT=0.5
 
 while True:
 	try:
 		sensor = minimalmodbus.Instrument('/dev/ttyUSB5', slaveaddress=ADDRESS)
+		# sensor.debug=True
+		# sensor.precalculate_read_size=False
 		print(sensor.read_register(0, functioncode=4), sensor.read_register(1, functioncode=4, numberOfDecimals=1, signed=True))
 		sleep(0.1)
 	except (IOError, ValueError):
